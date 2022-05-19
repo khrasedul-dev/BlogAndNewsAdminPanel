a996ed2f8ff88d58ef4e35518dfacec38709070f
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-05-19 17:28:19 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require("mongoose")
 const cors = require('cors')
-require('dotenv').config()
 const catRouter = require('./router/catRouter')
 const postRouter = require('./router/postRouter')
 const userRouter = require('./router/userRouter')
